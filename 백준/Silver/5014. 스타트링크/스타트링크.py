acd2c9ffe789562ffd90366d5752f2e513d3402a
kyusangshim <90396049+kyusangshim@users.noreply.github.com>
2025-10-28 10:41:00 +0900

    [Silver I] Title: 스타트링크, Time: 172 ms, Memory: 125784 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -3,30 +3,26 @@ from collections import deque
 F, S, G, U, D = map(int, input().split())
 
 visit = [0] * (F+1)
-count = 0
 
 def bfs(start):
-  # 큐 설정
+  # 큐 선언
   queue = deque()
   queue.append((start, 0))
-
-  # 방문 처리
   visit[start] = 1
 
-  # 레벨 순회
+  # 반복 순회 
   while queue:
-    current, count = queue.popleft()
+    stair, count = queue.popleft()
 
-    # 종료 조건
-    if current == G:
-      return count
-
-    for next in [current + U, current - D]:
-      if 1 <= next <= F and not visit[next]:
-        # 방문 처리
+    if stair == G:
+        return count
+    
+    for next in [stair+U, stair-D]:
+      if 1 <= next <= F and visit[next] == 0:
         visit[next] = 1
-        queue.append((next, count + 1))
-
+        queue.append((next, count+1))
+  
   return "use the stairs"
+
 
 print(bfs(S))
