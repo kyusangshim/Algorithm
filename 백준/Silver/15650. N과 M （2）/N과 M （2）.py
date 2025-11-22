b7ce5bd40e42ecf733439acacf030e3908a6e398
kyusangshim <90396049+kyusangshim@users.noreply.github.com>
2025-11-22 17:13:01 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,23 +1,15 @@
 N, M = map(int, input().split())
 
 stack = []
-visited = [False] * (N+1)
 
-def dfs(threshold):
-  if len(stack) == M:
-    print(' '.join(map(str, stack)))
-    return
-
-  for i in range(1, N+1):
-    if i < threshold:
-      continue
-    if not visited[i]:
-      visited[i] = True
-      stack.append(i)
-
-      dfs(i+1)
-
-      stack.pop()
-      visited[i] = False
-     
+def dfs(start):
+    if len(stack) == M:
+        print(' '.join(map(str, stack)))
+        return
+    
+    for i in range(start, N+1):
+        stack.append(i)
+        dfs(i+1)
+        stack.pop()
+        
 dfs(1)
