599206e5232505f75e8081af9b730e628e04952e
kyusangshim <90396049+kyusangshim@users.noreply.github.com>
2026-02-15 20:54:05 +0900

    [level 2] Title: 프로세스, Time: 0.23 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public static int findMax(int[] arr) {
+        int currentMax = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (currentMax < arr[i]) currentMax = arr[i];
+        }
+        return currentMax;
+    }
+    
+    public static int findIdx(int len, int idx) {
+        idx++;
+        return idx % len;
+    }
+    
+    public int solution(int[] priorities, int location) {
+        
+        int n = priorities.length;
+        int maxVal = findMax(priorities);
+        int idx = 0;
+        int count = 0;
+        
+        while (true) {            
+            if (maxVal == priorities[idx]) {
+                count++;
+                if (idx == location) {
+                    return count;
+                }
+                else {
+                    priorities[idx] = 0;
+                    idx = findIdx(n, idx);
+                    maxVal = findMax(priorities);
+                }
+            } else {
+                idx = findIdx(n, idx);
+                continue;
+            }
+        }
+        
+    }
+}
